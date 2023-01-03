c493c17c3e34e35d01f9640a9c311ad62da1a9ec
kneelian <3547240@gmail.com>
2023-01-03 01:35:48 +0100

    x

--- k_fractal.s
+++ k_fractal.s
@@ -4,20 +4,29 @@
 	clr4 x0, x1, x2, x3
 	//psh  x4
 
+	add x2, xzr, 1
+
 	mov x3, 0xC000
 	lsl x3, x3, 4
 
 	_fractal_loop:
-		
 		cmp  x0, 1024
 		b.eq _f_x0_toobig
 		_f_resume:
 
+		psh x2
+		bl _hash_3r_a_64b
+		pop x2
 
+		str w0, [sp, -4]!
+		str w1, [sp, -4]!
+		str w2, [sp, -4]!
+		bl _drawpx
+		add sp, sp, 12
 
+		add  x0, x0, 1
 		sub  x3, x3, 1
 		cbnz x3, _fractal_loop
-
 
 	//pop  x4
 	pop2 x2, x3
