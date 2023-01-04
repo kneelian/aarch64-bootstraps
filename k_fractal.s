af5adbd416ad993435d13d49dd572c5d87f122a4
kneelian <3547240@gmail.com>
2023-01-04 05:21:45 +0100

    s

--- k_fractal.s
+++ k_fractal.s
@@ -15,12 +15,16 @@
 		psh x2
 		bl _hash_3r_a_64b
 		pop x2
-		mov x4, x2
-		and x4, x4, 1
+		and x4, x2, 1
 		cbz x4, _f_skip
 		lsr x2, x2, 12
 		_f_skip:
 		add x2, x2, 1
+
+		and x4, x2, 0xff
+		add x2, x2, x4
+		add x2, x2, x4
+		add x2, x2, x4
 
 		str w0, [sp, -4]!
 		str w1, [sp, -4]!
